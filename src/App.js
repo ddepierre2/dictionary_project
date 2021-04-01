b7639dd15be2998dfb9d7c4482c063f23dcfae41
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-04-01 16:45:42 -0400

    Created Search form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <h1>Dictionary</h1>
        <h2>Type a word to look up in the dictionary</h2>
       </header>
+      <main>
+        <Dictionary />
+      </main>
 
-
-
-
-
-      <footer className="signature"><a href="https://github.com/ddepierre2/dictionary_project" 
+      <footer className="signature"><small><a href="https://github.com/ddepierre2/dictionary_project" 
       target="_blank" rel="noreferrer">Open source code</a> created by  
       <a href="https://www.linkedin.com/in/ddepierre/" 
       target="_blank" rel="noreferrer"> Dana De Pierre</a>
-      </footer>
+      </small></footer>
+    </div>
     </div>
   );
 }
