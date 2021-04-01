62378516ed55b26d9a75868bbdaa64bf80c395ce
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-04-01 16:06:10 -0400

    updated header and footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ export default function App() {
     <div className="App">
       <header className="App-header">
        <h1>Dictionary</h1>
+       <h2>Type a word to look up in the dictionary</h2>
       </header>
+
+
+
+
+
+      <footer className="signature"><a href="https://github.com/ddepierre2/dictionary_project" 
+      target="_blank" rel="noreferrer">Open source code</a> created by  
+      <a href="https://www.linkedin.com/in/ddepierre/" 
+      target="_blank" rel="noreferrer"> Dana De Pierre</a>
+      </footer>
     </div>
   );
 }
