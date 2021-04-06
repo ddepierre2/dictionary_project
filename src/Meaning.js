a4abe45120affcc50c220f41d5e15af0fe92ffa7
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-04-06 15:57:11 -0400

    Added Phonetic component

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,11 +2,9 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning (props){
-console.log(props.meaning);
 return (
   <div className="Meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
-
     {props.meaning.definitions.map(function(definition, index) 
     {
       return (
@@ -16,7 +14,6 @@ return (
           <br />
           <em><strong>Example: </strong>{definition.example}</em>
           <br />
-          <strong>Synonyms: </strong>
           <Synonyms synonyms={definition.synonyms} />
           </p>
         </div>
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 
 export default function Results (props){
@@ -9,6 +10,14 @@ return (
       <h2>
         {props.results.word}
       </h2>
+       {props.results.phonetics.map(function(phonetic, index){
+         return(
+           <div key={index}>
+             <Phonetic phonetic={phonetic} />
+            </div>
+         );
+       })}
+      
       {props.results.meanings.map(function(meaning, index){
         return (
         <div key={index}>
