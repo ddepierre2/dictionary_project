d02ebd206c309f8a11f31441fac975fb3b1be59c
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-04-06 18:21:55 -0400

    Created Photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results"
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 
 export default function Dictionary (){
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState (null);
+  let [photos, setPhotos] = useState (null);
    
 function handleResponse(response){
     setResults(response.data[0]);
+}
+function handleResponsePexels(response){
+setPhotos(response.data.photos);
 }
 
   function search(event){
      event.preventDefault();
       let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
        axios.get(apiUrl).then(handleResponse);
+
+     let pexelsApiKey="563492ad6f91700001000001dd3c45dc055d446fb7f442a2aaa200cc";
+     let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=15`;
+     let headers = {Authorization: `Bearer ${pexelsApiKey}` }
+     axios.get(pexelsApiUrl, { headers: headers }).then(handleResponsePexels);
     }
 
     
@@ -38,6 +48,7 @@ function handleResponse(response){
   </form>
   </section>
   <Results results={results} />
+  <Photos photos={photos} />
 
   </div>
 )
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactAudioPlayer from "react-audio-player";
+import "./Phonetic.css";
 
 export default function Phonetic (props){
-  console.log(props.phonetic);
   return(
   <div className="Phonetic">
     <ReactAudioPlayer
   src= {props.phonetic.audio}
+  className="audio-player"
   onPlay
   controls
 />
